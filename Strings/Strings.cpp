a8d7fcc97a48ae39be2608b43cac4f680d894214
З - Преподаватель < - @DESKTOP-PQFR2I3>
2023-11-07 14:48:58 +0500

    stream output example + task

--- Strings/Strings.cpp
+++ Strings/Strings.cpp
@@ -1,9 +1,15 @@
 ﻿// Strings.cpp : 
 #include <iostream>
+#include <string>
 #include <sstream>
-#include <format>
+#include <iomanip>
 
+#include <format>
+#include <vector>
 #include <cstring>
+
+
+//R"(text)"
 
 const char text[]{ R"(My mistress' eyes are nothing like the sun,
 Coral is far more red, than her lips red,
@@ -67,7 +73,8 @@ int Find(const char* str, const char* fnd) {
 
 }
 
-int main(){
+// json, xml, ini, csv
+int main2(){
 	const char strTest[] = "qw\tter ers\ner ty";
 	const char src[] = "er";
 	const char strTes_[]{ 'q','w','e','r','t','y'};
@@ -79,3 +86,104 @@ int main(){
 
 	return 0;
 }
+
+int main3() {
+	std::stringstream stream(text);
+	std::vector<std::string> words;
+
+	while (!stream.eof()) {
+		std::string tmp;
+		stream >> tmp;
+		words.push_back(tmp);
+	}
+
+	std::cout << "Was found " << words.size() << " words:\n";
+	for (int i = 0; i < words.size(); i++){
+		std::cout << std::setw(3) << i + 1 << ": " <<
+			words[i] << '\n';
+	}
+
+
+	return 0;
+}
+
+int main4() {
+	std::stringstream stream(text);
+	std::vector<std::string> lines;
+
+	while (!stream.eof()) {
+		std::string tmp;
+		std::getline(stream,tmp,'\n');
+		lines.push_back(tmp);
+	}
+
+	std::cout << "Was found " << lines.size() << " lines:\n";
+	for (int i = 0; i < lines.size(); i++) {
+		std::cout << std::setw(3) << i + 1 << ": " <<
+			lines[i] << '\n';
+	}
+
+	return 0;
+}
+
+/*
+	показать пользователю только уникальные слова в исходном тексте
+	
+	выбрать любое слово из текста, показать сколько раз оно
+	встречается в каждой строке
+
+*/
+
+//считать текст и заменить в нём какое либо слово на другое
+int main() {
+	std::stringstream t_stream(text);
+	std::string example{ "her" };
+	std::string replace{ "HER" };
+
+	std::string final_text;
+
+	std::stringstream t_ostream(final_text);
+	while(!t_stream.eof() ){
+		std::string tmp;
+		std::getline(t_stream, tmp, '\n');
+		std::stringstream words(tmp);
+		while (!words.eof()) {
+			std::string word;
+			words >> word;
+			if (word == example) {
+				t_ostream << replace;
+			}
+			else {
+				t_ostream << word;
+			}
+			t_ostream << ' ';
+		}
+		t_ostream << '\n';
+	}
+	//t_ostream.flush();
+	final_text = t_ostream.str();
+	std::to_string(3.5);
+	std::cout << text << "\n\n\n";
+	std::cout << final_text;
+	return 0;
+}
+
+const char mask[]{ "What answer have equation Y = K * X + C \n\
+1 - X \n\
+2 - A\n\
+3 - B\n\
+4 - Z\n" };
+
+/*
+Задача
+написать программу в которой пользователь вводит сколько нужно сгенирировать 
+задачь на решение линейных уравнений и макисмальные-мнимальные значения 
+коэффициентов уравнения
+
+после чего программа выводит на экран список задачь с вариантами ответов 
+согласно маске
+Y, K, C - генерируется. X - расчитывается
+A, B, Z - должны гарантированно отличаться от X
+
+
+*/
